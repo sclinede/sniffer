25205ca59893b8952a17bba31be3967bb99a4e5d
Sergey Dolganov <sclinede@gmail.com>
2018-08-09 14:03:06 +0300

    Patch HTTPClient with prepend

--- lib/sniffer/adapters/httpclient_adapter.rb
+++ lib/sniffer/adapters/httpclient_adapter.rb
@@ -4,56 +4,45 @@ module Sniffer
   module Adapters
     # HttpClient adapter
     module HTTPClientAdapter
-      def self.included(base)
-        base.class_eval do
-          alias_method :do_get_block_without_sniffer, :do_get_block
-          alias_method :do_get_block, :do_get_block_with_sniffer
-        end
-      end
-
-      # private
-
       # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-      def do_get_block_with_sniffer(req, proxy, conn, &block)
-        if Sniffer.enabled?
-          data_item = Sniffer::DataItem.new
-          data_item.request = Sniffer::DataItem::Request.new(host: req.header.request_uri.host,
-                                                             query: req.header.create_query_uri,
-                                                             method: req.header.request_method,
-                                                             headers: req.headers,
-                                                             body: req.body,
-                                                             port: req.header.request_uri.port)
+      def do_get_block(req, proxy, conn, &block)
+        return super(req, proxy, conn, &block) unless Sniffer.enabled?
 
-          Sniffer.store(data_item)
-        end
+        data_item = Sniffer::DataItem.new
+        data_item.request = Sniffer::DataItem::Request.new(host: req.header.request_uri.host,
+                                                           query: req.header.create_query_uri,
+                                                           method: req.header.request_method,
+                                                           headers: req.headers,
+                                                           body: req.body,
+                                                           port: req.header.request_uri.port)
+
+        Sniffer.store(data_item)
 
         retryable_response = nil
 
         bm = Benchmark.realtime do
           begin
-            do_get_block_without_sniffer(req, proxy, conn, &block)
+            super(req, proxy, conn, &block)
           rescue HTTPClient::RetryableResponse => e
             retryable_response = e
           end
         end
 
-        if Sniffer.enabled?
-          res = conn.pop
-          data_item.response = Sniffer::DataItem::Response.new(status: res.status_code.to_i,
-                                                               headers: res.headers,
-                                                               body: res.body,
-                                                               timing: bm)
+        res = conn.pop
+        data_item.response = Sniffer::DataItem::Response.new(status: res.status_code.to_i,
+                                                             headers: res.headers,
+                                                             body: res.body,
+                                                             timing: bm)
 
-          conn.push(res)
+        conn.push(res)
 
-          data_item.log
-        end
+        data_item.log
 
         raise retryable_response unless retryable_response.nil?
       end
-      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
+      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
     end
   end
 end
 
-HTTPClient.send(:include, Sniffer::Adapters::HTTPClientAdapter) if defined?(::HTTPClient)
+HTTPClient.send(:prepend, Sniffer::Adapters::HTTPClientAdapter) if defined?(::HTTPClient)
